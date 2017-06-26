a86873feb78897e2f0f48029bda08f3258be4b74
Kougra <renato.gremio@hotmail.com>
2017-06-26 20:41:51 -0300

    Cadastrei as Acess Keys e Consumer Keys corretamente, a aplicação está rodando!

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 var TwitterPackage = require('twitter');
 
 var secret = {
-  consumer_key: 'YOURS',
-  consumer_secret: 'YOURS',
-  access_token_key: 'YOURS',
-  access_token_secret: 'YOURS'
+  consumer_key: 'uedO2hKxmEqU5bCGKu7bxSOVd',
+  consumer_secret: 'TKG390SMOdU4BQhdYt9usfVpmOfZFqco5oMsU07F5ooSjYQf7B',
+  access_token_key: '72085746-1Fuoa6Ytraz6AF1UI46CMK40WlSgzbs55mVK51gnc',
+  access_token_secret: '6EqSmhYiVZnBv2Q7wVbIo84hFiUCm7siKtNUX4R4AeOqq'
 }
 
 var Twitter = new TwitterPackage(secret);
 
-Twitter.stream('statuses/filter', {track: '@kopzinksi'}, function(stream) {
+Twitter.stream('statuses/filter', {track: '@donald'}, function(stream) {
   stream.on('data', function(tweet) {
     console.log(tweet.text);
   });
