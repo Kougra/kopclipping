6a53696b2fc544e45a1672bccad8a8eca254c7b8
Kougra <renato.gremio@hotmail.com>
2017-07-18 19:38:24 -0300

    Criado um schema para ver os negativos do tweet referido

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var TwitterPackage = require('twitter');
+
 var mongoose = require('mongoose');
-//mongoose.connect('mongodb://localhost/test');
+
 mongoose.connect('mongodb://user5:123456@ds139942.mlab.com:39942/kopteste');
 
 // Twitter Develop. keys
@@ -10,22 +11,24 @@ var secret = {
   access_token_key: '72085746-1Fuoa6Ytraz6AF1UI46CMK40WlSgzbs55mVK51gnc',
   access_token_secret: '6EqSmhYiVZnBv2Q7wVbIo84hFiUCm7siKtNUX4R4AeOqq'
 }
- var Post = mongoose.model('Post', {mensagem: String, nome: String, localizaçao: String, seguidores: Number, amigos: Number, owner: {type: String, index: true}});
+
+  // Model post
+ var Post = mongoose.model('Post', {
+  mensagem: String, 
+  nome: String, 
+  localizaçao: String, 
+  seguidores: Number, 
+  amigos: Number, 
+  owner: {type: String, index: true}});
+
 
 var Twitter = new TwitterPackage(secret);
 
+// Filtro para buscar (katy perry)
 Twitter.stream('statuses/filter', {track: '@katyperry'}, function(stream) {
   stream.on('data', function(tweet) {
     //console.log(tweet.user.location, tweet.user.name, tweet.text, tweet.user.followers_count, tweet.user.friends_count , tweet.id);
 
-    var Person = mongoose.model('Person', yourSchema);
-
-    Person.findOne({ "name" : { $regex: /Ghost/, $options: 'i' } },
-          function (err, person) {
-                 if (err) return handleError(err);
-                 console.log('%s %s is a %s.', person.name.first, person.name.last, person.occupation);
-
-   });
 
     // Coincide com model Post (var)
     var postAtual = new Post({
@@ -48,7 +51,6 @@ Twitter.stream('statuses/filter', {track: '@katyperry'}, function(stream) {
     });
   });
   
-
   stream.on('error', function(error) {
     console.log(error);
   });
